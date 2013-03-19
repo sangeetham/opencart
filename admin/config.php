f3359b55f8b8e67d489d7737a8a1859a165a7f9d
sangeetham <sangeetham.ulugonda@hiddenbrains.in>
2013-03-19 14:52:14 +0530

    uploaded all files to git

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.36.27/opencartnew/admin/');
+define('HTTP_CATALOG', 'http://192.168.36.27/opencartnew/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.36.27/opencartnew/admin/');
+define('HTTPS_CATALOG', 'http://192.168.36.27/opencartnew/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencartnew/admin/');
+define('DIR_SYSTEM', '/var/www/opencartnew/system/');
+define('DIR_DATABASE', '/var/www/opencartnew/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencartnew/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencartnew/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencartnew/system/config/');
+define('DIR_IMAGE', '/var/www/opencartnew/image/');
+define('DIR_CACHE', '/var/www/opencartnew/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencartnew/download/');
+define('DIR_LOGS', '/var/www/opencartnew/system/logs/');
+define('DIR_CATALOG', '/var/www/opencartnew/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'hbdev');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
